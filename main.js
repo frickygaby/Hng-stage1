0c0bda5e3ed1ba27ef2c249ed4741c4f17963d1e
Gabriel Ndibe <ndibeekpere@gmail.com>
2023-09-11 22:02:03 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function setDayOfWeek() {
+    const dayOfWeekElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+    const currentDate = new Date();
+    const options = { weekday: 'long' };
+    const formattedDayOfWeek = currentDate.toLocaleDateString(undefined, options);
+    dayOfWeekElement.textContent = formattedDayOfWeek;
+}
+function updateClock() {
+    const timeElement = document.querySelector('[data-testid="currentUTCTime"]');
+    const updateClockDisplay = () => {
+        const currentDate = new Date();
+        const options = { hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
+        const formattedTime = currentDate.toLocaleTimeString(undefined, options);
+        timeElement.textContent = formattedTime;
+    };
+    updateClockDisplay();
+
+    setInterval(updateClockDisplay, 1000);
+}
+setDayOfWeek();
+updateClock();
